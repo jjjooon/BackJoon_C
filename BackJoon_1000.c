06d57e5cb0f8cc75cfa38531ef9d020b47b2839f
jjjooon <parkwhdrms98@naver.com>
2022-10-08 10:12:06 +0900

    Add Text File [BackJoon_1000.c]

---
+++ BackJoon_1000.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	
+	int A, B = 0;
+	scanf_s("%d", &A);
+	scanf_s("%d", &B);
+
+	int sum = A + B;
+
+	printf("%d",sum);
+	return 0;
+}
---
+++ BackJoon_1001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+	int A, B = 0;
+	scanf_s("%d", &A);
+	scanf_s("%d", &B);
+
+	printf("%d", A - B);
+	return 0;
+}
